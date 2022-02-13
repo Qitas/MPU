57c14aec62c61812ffaac1010b64f0bca8a49706
qitas <qitas@qitas.cn>
2022-02-13 18:41:41 +0800

    nothing

--- conf.py
+++ conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Qitas'
-copyright = u'2021, docs.qitas.cn'
+copyright = u'2022, www.qitas.cn'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
